a02b4fff0428bf68deca18e1fc89b5d3f75ce018
jazminfern4ndez <esmeraldafernandez547@gmail.com>
2025-09-02 18:07:23 +0000

    Actividad Poo: Minibanco inicial

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Cliente cliente1= new Cliente ("Luis Gomez",25672946);
         CuentaBancaria cuenta1 = new CuentaBancaria(6831531,cliente1,10000);
+
+        CuentaBancaria cuenta20 new CuentaBancaria(123,new cliente("Juan Perez",234),1200);
     }
 }
